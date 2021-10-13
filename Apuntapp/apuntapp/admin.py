246d62f67227e001ce9594f7ea2fee73688123ee
LmGra <garcia-rincon-arauz@hotmail.es>
2021-10-13 18:33:20 +0200

    Commit 5

--- Apuntapp/apuntapp/admin.py
+++ Apuntapp/apuntapp/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Autor, Temario, Publicacion, Modulo
 
 admin.site.register(Autor)
-admin.site.register(Temario)
+admin.site.register(Asignatura)
 admin.site.register(Publicacion)
-admin.site.register(Modulo)
+admin.site.register(Ciclo)
 #admin.site.register(Asignatura)
--- Apuntapp/apuntapp/models.py
+++ Apuntapp/apuntapp/models.py
@@ -9,36 +9,44 @@ class Autor(models.Model):
     def __str__(self):
         return self.nickname
 
-class Temario(models.Model):
+class Asignatura(models.Model):
 
-    modulotemario = models.ForeignKey("Modulo", on_delete=models.CASCADE, null=True)
+    CicloAsignatura = models.ForeignKey("Ciclo", on_delete=models.CASCADE, null=True)
 
     LISTA_ASIGNATURA = [
-        ("IAW+HLC","IAW+HLC"),
-        ("SAD","SAD"),
-        ("ASGBD","ASGBD"),
-        ("SRI","SRI"),
-        ("ASO","ASO"),
-        ("EIE","EIE"),
-        ("PAR","PAR"),
-        ("FH","FH"),
-        ("GBD","GBD"),
-        ("ISO","ISO"),
-        ("LMSGI","LMSGI"),
-        ("FOL","FOL"),
-        ("SI","SI"),
-        ("BD","BD"),
-        ("PROG","PROG"),
-        ("ED","ED"),
-        ("AD","AD"),
-        ("DI","DI"),
-        ("PMDM","PMDM"),
-        ("PSP","PSP"),
-        ("SGE","SGE"),
+        ("IAW+HLC","Implantación de Aplicaciones Web + Hora libre configuración"),
+        ("SAD","Seguridad y Alta Disponibilidad"),
+        ("ASGBD","Administración de Sistemas Gestores de Bases de Datos"),
+        ("SRI","Servicios de Red e Internet"),
+        ("ASO","Administración de Sistemas Operativos"),
+        ("EIE","Empresa e Iniciativa Emprendedora"),
+        ("PAR","Planificación y Administración de Redes"),
+        ("FH","Fundamentos Hardware"),
+        ("GBD","Gestión de Bases de Datos"),
+        ("ISO","Implantación de Sistemas Operativos"),
+        ("LMSGI","Lenguaje de Marcas y Sistemas de Gestión de información"),
+        ("FOL","Formación y Orientación Laboral"),
+        ("SI","Sistemas Informáticos"),
+        ("BD","Base de Datos"),
+        ("PROG","Programación"),
+        ("ED","Entorno de Desarrollo"),
+        ("AD","Acceso a Datos"),
+        ("DI","Desarrollo de Interfaces"),
+        ("PMDM","Programación Multimedia y Dispositivos Móviles"),
+        ("PSP","Programación de Servicios y Procesos"),
+        ("SGE","Sistemas de Gestión Empresarial"),
+        ("MME","Montaje y Mantenimiento de Equipos"),
+        ("SOM","Sistemas Operativos Monopuesto"),
+        ("AO","Aplicaciones Ofimáticas"),
+        ("RL","Redes Locales"),
+        ("SOR","Sistemas Operativos en Red"),
+        ("SI","Seguridad Informática"),
+        ("SR","Servicios en Red"),
+        ("AW","Aplicaciones Web")
 
     ]
 
-    asignatura = models.CharField('Asignatura', max_length=12, choices=LISTA_ASIGNATURA, blank=False, default="IAW+HLC")
+    asignatura = models.CharField('Asignatura', max_length=64, choices=LISTA_ASIGNATURA, blank=False, default="IAW+HLC")
     tema = models.DecimalField('Tema', max_digits=2, decimal_places=0, default=1)
 
 
@@ -49,24 +57,25 @@ class Publicacion(models.Model):
     titulo = models.CharField(max_length=50,blank=False,default="")
     texto = models.CharField(max_length=9999,null=True,blank=True)
     archivoFichero = models.FileField(upload_to="archivos/", null=True, blank=True)
-    subida = models.ForeignKey("Temario", on_delete=models.CASCADE, null=True)
+    subida = models.ForeignKey("Asignatura", on_delete=models.CASCADE, null=True)
     autorpublica = models.ForeignKey("Autor", on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.titulo
 
-class Modulo(models.Model):
-    LISTA_MODULO = [
-        ("ASIR","ASIR"),
-        ("DAM","DAM")
+class Ciclo(models.Model):
+    LISTA_Ciclo = [
+        ("ASIR","Administración de Sistemas Informáticos en Red"),
+        ("DAM","Desarrollo de Aplicaciones Multiplataforma"),
+        ("SMR","Sistemas Microinformáticos y Redes")
     ]
 
-    nombremodulo = models.CharField('NombreModulo', max_length=12, choices=LISTA_MODULO, blank=False, default="ASIR")
+    nombreCiclo = models.CharField('NombreCiclo', max_length=12, choices=LISTA_Ciclo, blank=False, default="ASIR")
 
     def __str__(self):
-        return self.nombremodulo
+        return self.nombreCiclo
 
-#class Temario(models.Model):
-#    nomTemario = models.CharField(max_length=16)
+#class Asignatura(models.Model):
+#    nomAsignatura = models.CharField(max_length=16)
 #class Asignatura(models.Model):
 #    nomAsignatura = models.CharField(max_length=16)
