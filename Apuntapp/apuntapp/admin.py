fb7747d91f2ac09869e8b033f5d75fc15f123c7c
LmGra <garcia-rincon-arauz@hotmail.es>
2021-10-13 18:34:31 +0200

    Commit 5_5

--- Apuntapp/apuntapp/admin.py
+++ Apuntapp/apuntapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Autor, Temario, Publicacion, Modulo
+from .models import Autor, Asignatura, Publicacion, Ciclo
 
 admin.site.register(Autor)
 admin.site.register(Asignatura)
