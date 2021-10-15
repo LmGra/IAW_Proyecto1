d9ac5e623901efc185b35b66ff9e8c4e32667984
LmGra <garcia-rincon-arauz@hotmail.es>
2021-10-15 17:12:58 +0200

    Commit 6

--- Apuntapp/apuntapp/urls.py
+++ Apuntapp/apuntapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+from apuntapp.views import PublisherListView
+
 urlpatterns = [
+    #path('PublisherListView/', PublisherListView.as_view())
     path('', views.index, name='index'),
 ]
--- Apuntapp/apuntapp/views.py
+++ Apuntapp/apuntapp/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.views.generic import ListView
+from apuntapp.models import Autor, Modulo, Publicacion, Ciclo
+
+class AutorListView(ListView):
+    model = Autor
+class ModuloListView(ListView):
+    model = Modulo
+class PublicacionListView(ListView):
+    model = Publicacion
+class CicloListView(ListView):
+    model = Ciclo
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+#from django.shortcuts import render
+
+#from django.http import HttpResponse
+
+
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the polls index.")
