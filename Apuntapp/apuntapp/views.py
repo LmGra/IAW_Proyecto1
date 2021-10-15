7303b55054b6f63149ac8b9338d02e616161b832
LmGra <garcia-rincon-arauz@hotmail.es>
2021-10-15 21:30:28 +0200

    Commit 7

--- Apuntapp/apuntapp/views.py
+++ Apuntapp/apuntapp/views.py
@@ -1,6 +1,7 @@
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from apuntapp.models import Autor, Modulo, Publicacion, Ciclo
+
 
 class AutorListView(ListView):
     model = Autor
@@ -10,6 +11,10 @@ class PublicacionListView(ListView):
     model = Publicacion
 class CicloListView(ListView):
     model = Ciclo
+
+class PublicacionDetailView(DetailView):
+    model = Publicacion
+
 
 
 #from django.shortcuts import render
